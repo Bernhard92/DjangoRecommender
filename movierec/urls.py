3814b2951e5426fda6622db286938bdbdc1a4ce4
bernhardCS <b.jahrer@gmx.net>
2019-05-19 23:23:57 +0200

    basic gui structure added

---
+++ movierec/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'movierec'
+urlpatterns = [
+	# ex: /movierec/
+	#path('', views.IndexView.as_view(), name='index'),
+	path('', views.index, name='index')
+
+	# ex: /movierec/5/
+	#path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
+	# ex: /movierec/5/results/
+	#path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
+	# ex: /movierec/5/vote/
+	#path('<int:question_id>/vote/', views.vote, name='vote'),
+	
+]
--- movierec/views.py
+++ movierec/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views import generic
+from django.template import loader
+
+from .models import User, Movie, Genre, Rating
 
 # Create your views here.
+def index(request):
+	"""landing page"""
+	template = loader.get_template('movierec/index.html')
+
+	movieList = [["m1", "beschreibung1", 2001], ["m2", "beschreibung2", 2011], ["m3", "beschreibung3", 2005], ["m4", "beschreibung2", 2011], ["m5", "beschreibung3", 2005]]
+
+	recommended_list_1 = tuple(movieList)
+	recommended_list_2 = tuple(movieList)
+	recommended_list_3 = tuple(movieList)
+	recommended_list_4 = tuple(movieList)
+	recommended_list_5 = tuple(movieList)
+
+	
+	recommendation_lists = [recommended_list_1, recommended_list_2, recommended_list_3, recommended_list_4, recommended_list_5]
+	context = {'recommendation_lists': recommendation_lists}
+ 
+	return render(request, 'movierec/index.html', context)
+
+
+	
+
+
+	
