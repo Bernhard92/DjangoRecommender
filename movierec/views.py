a0f3443c26ef3af89a386769a25b03afdb919d4c
bernhardCS <b.jahrer@gmx.net>
2019-05-20 12:38:05 +0200

    added horizontal scrolling

--- movierec/views.py
+++ movierec/views.py
@@ -9,7 +9,19 @@ def index(request):
 	"""landing page"""
 	template = loader.get_template('movierec/index.html')
 
-	movieList = [["m1", "beschreibung1", 2001], ["m2", "beschreibung2", 2011], ["m3", "beschreibung3", 2005], ["m4", "beschreibung2", 2011], ["m5", "beschreibung3", 2005]]
+	movieList = [
+		["m1", "beschreibung1", 2001], 
+		["m2", "beschreibung2", 2011], 
+		["m3", "beschreibung3", 2005], 
+		["m4", "beschreibung4", 2011], 
+		["m5", "beschreibung5", 2005],
+		["m6", "beschreibung6", 2001], 
+		["m7", "beschreibung7", 2011], 
+		["m8", "beschreibung8", 2005], 
+		["m9", "beschreibung9", 2011], 
+		["m10", "beschreibung10", 2005],
+
+	]
 
 	recommended_list_1 = tuple(movieList)
 	recommended_list_2 = tuple(movieList)
