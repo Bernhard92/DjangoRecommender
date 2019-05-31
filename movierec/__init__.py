9b90489fa9cec5618a7aa7348e2414dbe8b1536b
markus-fox <markus.fox@aau.at>
2019-05-31 16:50:27 +0200

    Added movies-dataset and index test

---
+++ movierec/__init__.py
@@ -0,0 +1,15 @@
+# initialize poster recommender at startup
+import numpy as np
+import os
+import pandas as pd
+from skimage.transform import resize
+from skimage import io
+from movierec.poster_knn import PosterKNN
+
+print('starting to load poster data for recommender')
+image_data_feats = np.load(os.path.join(os.getcwd(),'movierec/data/image_features_X.npy'))
+image_data_y = np.load(os.path.join(os.getcwd(),'movierec/data/image_ids_y.npy'))
+
+poster_recommender = PosterKNN()
+poster_recommender.train(image_data_feats,image_data_y)
+print('initialized poster recommender')
--- movierec/views.py
+++ movierec/views.py
@@ -2,26 +2,27 @@ from django.shortcuts import render
 from django.views import generic
 from django.template import loader
 from .models import User, Movie, Genre, Rating
+from . import poster_recommender
 
 # Create your views here.
 def index(request):
 	"""landing page"""
 	template = loader.get_template('movierec/index.html')
 
-	user_movie_title = request.GET.get('movie_title','Star Wars') 
+	user_movie_title = request.GET.get('movie_title','Star Wars')
 
-	
+	similar = poster_recommender.recommend(float(user_movie_title),10)
 
 	movieList = [
-		["m1", "beschreibung1", 2001], 
-		["m2", "beschreibung2", 2011], 
-		["m3", "beschreibung3", 2005], 
-		["m4", "beschreibung4", 2011], 
-		["m5", "beschreibung5", 2005],
-		["m6", "beschreibung6", 2001], 
-		["m7", "beschreibung7", 2011], 
-		["m8", "beschreibung8", 2005], 
-		["m9", "beschreibung9", 2011], 
+		["m1", "beschreibung1", similar[0]],
+		["m2", "beschreibung2", similar[1]],
+		["m3", "beschreibung3", similar[2]],
+		["m4", "beschreibung4", similar[3]],
+		["m5", "beschreibung5", similar[4]],
+		["m6", "beschreibung6", 2001],
+		["m7", "beschreibung7", 2011],
+		["m8", "beschreibung8", 2005],
+		["m9", "beschreibung9", 2011],
 		["m10", "beschreibung10", 2005],
 
 	]
@@ -39,9 +40,3 @@ def index(request):
 		'movie_title': user_movie_title
 	}
 	return render(request, 'movierec/index.html', context)
-
-
-	
-
-
-	
